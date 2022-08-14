af9acef7710c9af25e4da4231ea03e6174bd0ef2
kimekyoung <ek970821111@gmail.com>
2022-08-14 16:03:12 +0900

    주석 추가

--- basic_example/example01.js
+++ basic_example/example01.js
@@ -4,10 +4,13 @@
 var username = "홍길동";
 var username = "둘리";
 console.log(username);
+// 특별한 경우 아니면잘 안씀
 
 let age = 14;
 age = 20;
 console.log(age);
+// 변경이 가능함
 
 const gender = "female";
 console.log(gender);
+// 변경이 불가능함
