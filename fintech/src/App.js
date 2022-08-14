8132f4cd96e4506cfa8f1f5ec695dc1b29caf108
kimekyoung <ek970821111@gmail.com>
2022-08-14 19:07:46 +0900

    Input example 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,15 +1,13 @@
 
 import './App.css';
+import InputExample from './components/InputExample';
 import Welecome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <h1>안녕하세요</h1>
-      <Welecome username={"유관우"} age={33}></Welecome>
-      <Welecome username={"홍길동"} age={33}></Welecome>
-      <Welecome username={"둘리"} age={33}></Welecome>
-      <Welecome username={"도우너"} age={33}></Welecome>
+      <InputExample></InputExample>
     </div>
   );
 }
